095be94086206c71b62890fc70329cacc869d694
fishsticks89 <61331006+fishsticks89@users.noreply.github.com>
2021-11-09 19:10:54 -0800

    a short test to see if message passing works 😯

--- src/meia/drive/chassis_controller.cpp
+++ src/meia/drive/chassis_controller.cpp
@@ -4,7 +4,9 @@ namespace meia {
         int delay_time = ((Pid_task_messenger_struct*)p)->drive_task_delay_factor;
         std::string initmessage = "pid initiated";
         printf(initmessage.c_str());
+        std::string printmessage = initmessage + " " + std::to_string(delay_time);
         while (true) {
+            printf(printmessage.c_str());
             pros::delay(1000 * delay_time);
         }
     }
