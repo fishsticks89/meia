8430535da99edda01925b9789f1f4b8e33c96d72
fishsticks89 <61331006+fishsticks89@users.noreply.github.com>
2021-11-10 20:46:16 -0800

    a test for a task suspending itself 👹

--- include/meia/drive/chassis_controller.hpp
+++ include/meia/drive/chassis_controller.hpp
@@ -20,17 +20,32 @@ namespace meia {
      *      If your drive is 36:60 where the 60t is powered, your RATIO would be 0.6.
      */
     class ChassisController: private Chassis {
-        private:        
-            double ticks_per_inch;
-            std::pair<double, double> set_voltage_normalized(double l_volt, double r_volt);
+        private:
             struct Pid_task_messenger_struct {
-                Pid_task_messenger_struct(int p_drive_task_delay_factor) : drive_task_delay_factor{p_drive_task_delay_factor} {}
+                /**
+                 * Struct used to pass messages to pid_task
+                 * \param self_ptr
+                 *      a pointer to the task so that it can suspend itself during joystick control
+                 * \param is_inactive
+                 *      a pointer to a bool where, if set to true, pid_task will suspend itself
+                 * \param drive_task_delay_factor
+                 *      how many centiseconds the drive waits 
+                */
+                Pid_task_messenger_struct(pros::Task* self_ptr, bool* is_inactive, int drive_task_delay_factor) : 
+                    self_ptr{self_ptr}, 
+                    is_inactive{is_inactive},
+                    drive_task_delay_factor{drive_task_delay_factor} {}
+                pros::Task* self_ptr; // used to suspend task during joystick control
+                bool* is_inactive; // same as above
                 int drive_task_delay_factor = 1;
+                void set_voltage();
             };
             pros::Task pid_loop_task;
-            static void pid_loop(void* p);
-        public:
+            static void pid_loop(void* p);        
+            double ticks_per_inch;
+            void set_voltage_normalized(double l_volt, double r_volt);
             Pid_task_messenger_struct pid_task_messenger;
+        public:
             explicit ChassisController(std::vector<int> left_motors, std::vector<int> right_motors, double wheel_diameter, int motor_rpm, double gear_ratio, int delay_time = 1) : 
             Chassis(left_motors, right_motors),
             ticks_per_inch(
@@ -38,10 +53,11 @@ namespace meia {
                     /
                 (wheel_diameter * M_PI) // Circumference of wheel
             ),
-            pid_task_messenger(delay_time),
+            pid_task_messenger(&pid_loop_task, &joy_control, delay_time),
             pid_loop_task(pid_loop, &pid_task_messenger, "pid_task")
             {};
-            // uses tank control and get_temps function from meia::Chassis
+            // expose functions from meia::Chassis
+            using Chassis::joy_control;
             using Chassis::tank_control;
             using Chassis::get_motor_temps;
     };
